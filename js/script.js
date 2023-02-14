e8042d2ee357986bd73fda9bc98984e66402b748
fracs31 <pist.fracs95@gmail.com>
2023-02-14 15:43:54 +0100

    ADD cards

--- js/script.js
+++ js/script.js
@@ -31,8 +31,30 @@ const people = [
         img: "./img/barbara-ramos-graphic-designer.jpg" //immagine
     }
 ];
+const rowElement = document.getElementById("row"); //riga nel DOM
+let colElement = ""; //colonna
 
 //Ciclo
 for (let key in people) {
     console.log(people[key]); //stampo gli oggetti dell'array di persone
 }
+
+//Creo le colonne con le carte
+for (let i = 0; i < people.length; i++) {
+    let fullName = people[i].fullName; //nome completo
+    let role = people[i].role; //ruolo
+    let img = people[i].img; //immagine
+    colElement += "<div class='col'>"; //inzio colonna
+    colElement += "<div class='card'>"; //inizio carta
+    colElement += "<div class='card-head'>"; //inizio testa della carta
+    colElement += "<img src='" + img + "' alt='" + fullName + "'>"; //immagine
+    colElement += "</div>"; //fine testa della carta
+    colElement += "<div class='card-body'>"; //inizio corpo della carta
+    colElement += "<h2>" + fullName + "</h2>"; //nome
+    colElement += "<h3>" + role + "</h3>"; //ruolo
+    colElement += "</div>"; //fine corpo della carta
+    colElement += "</div>"; //fine carta
+    colElement += "</div>"; //fine colonna
+}
+
+rowElement.innerHTML += colElement; //stampo le colonne con le carte
