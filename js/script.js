5b8f3e5de62177801a2d42561ef715db9c735d1f
fracs31 <pist.fracs95@gmail.com>
2023-02-14 15:02:25 +0100

    ADD array of people

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Array di persone
+const people = [
+    {
+        fullName: "Wayne Barnett", //nome completo
+        role: "Founder & CEO", //ruolo
+        img: "./img/wayne-barnett-founder-ceo.jpg" //immagine
+    },
+    {
+        fullName: "Angela Caroll", //nome completo
+        role: "Chief Editor", //ruolo
+        img: "./img/angela-caroll-chief-editor.jpg" //immagine
+    },
+    {
+        fullName: "Walter Gordon", //nome completo
+        role: "Office Manager", //ruolo
+        img: "./img/walter-gordon-office-manager.jpg" //immagine
+    },
+    {
+        fullName: "Angela Lopez", //nome completo
+        role: "Social Media Manager", //ruolo
+        img: "./img/angela-lopez-social-media-manager.jpg" //immagine
+    },
+    {
+        fullName: "Scott Estrada", //nome completo
+        role: "Developer", //ruolo
+        img: "./img/scott-estrada-developer.jpg" //immagine
+    },
+    {
+        fullName: "Barbara Ramos", //nome completo
+        role: "Graphic Designer", //ruolo
+        img: "./img/barbara-ramos-graphic-designer.jpg" //immagine
+    }
+]
